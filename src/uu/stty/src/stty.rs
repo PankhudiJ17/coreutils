ac865d7bfca7825c2c3c69e049423c165625f9b0
Pankhudi Jain <Pankhudi.Jain@ibm.com>
2026-02-06 12:03:27 +0530

    Changed stty and flags, comented sscache

--- src/uu/stty/src/stty.rs
+++ src/uu/stty/src/stty.rs
@@ -22,7 +22,11 @@ use clap::{Arg, ArgAction, ArgMatches, Command};
 use nix::libc::{O_NONBLOCK, TIOCGWINSZ, TIOCSWINSZ, c_ushort};
 
 //#[cfg(target_os = "linux")]
-#[cfg(any(not(target_os = "linux"), target_arch = "powerpc64"))]
+//#[cfg(any(not(target_os = "linux"), target_arch = "powerpc64"))]
+#[cfg(all(
+    target_os = "linux",
+    not(all(target_arch = "powerpc64", target_endian = "little"))
+))]
 use nix::libc::{TCGETS2, termios2};
 
 use nix::sys::termios::{
