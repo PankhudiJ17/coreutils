341279d562749063425669f7cce13e4f5e081f44
Pankhudi Jain <Pankhudi.Jain@ibm.com>
2026-02-10 11:11:34 +0530

    adec cond test_cp.rs

--- tests/by-util/test_cp.rs
+++ tests/by-util/test_cp.rs
@@ -76,13 +76,14 @@ macro_rules! assert_metadata_eq {
       //  if cfg!(all(target_arch = "powerpc64", target_endian = "little")) {
       //  if cfg!(all(target_os = "linux", not(target_arch = "powerpc64"))) {    
        // if !cfg!(all(target_os = "linux", target_arch = "powerpc64")) {
+        if cfg(all(target_os = "linux", not(target_arch = "powerpc64"))) {    
         assert_eq!($m1.atime(), $m2.atime(), "atime is different");
         assert_eq!(
             $m1.atime_nsec(),
             $m2.atime_nsec(),
             "atime_nsec is different"
         );
-       // }  
+        }  
         assert_eq!($m1.mtime(), $m2.mtime(), "mtime is different");
         assert_eq!(
             $m1.mtime_nsec(),
